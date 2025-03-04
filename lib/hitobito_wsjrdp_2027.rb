3c9263c34068dc87ba3864028a5d9fe5b3769303
Peter Neubauer <development@smeky.de>
2025-03-04 13:15:31 +0100

    initial wagon wsjrdp_2027

---
+++ lib/hitobito_wsjrdp_2027.rb
@@ -0,0 +1,6 @@
+require 'wagons'
+# require 'your_wagon_dependencies'
+require 'hitobito_wsjrdp_2027/wagon'
+
+module HitobitoWsjrdp2027
+end
---
+++ test/hitobito_wsjrdp_2027_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HitobitoWsjrdp2027Test < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, HitobitoWsjrdp2027
+  end
+end
