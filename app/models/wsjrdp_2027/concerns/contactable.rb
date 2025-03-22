02646544b530dc30766c6e470ec913525a70e436
Felix Maurer <felix@felix-maurer.de>
2025-03-22 01:23:05 +0100

    Replace copied Contactable with prepended module
    Instead of maintaining a copy of Contactable in our code, we define a small
    module that overrides just the method providing the default zip code
    country. As we are overriding a method defined in the Contactable module,
    we have to use prepend to insert our module in the beginning of the
    ancestor chain, i.e., adding it before the original Contactable to the
    function lookup order.

---
+++ app/models/wsjrdp_2027/concerns/contactable.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Wsjrdp2027::Concerns::Contactable
+
+  # Overriding zip_country from Hitobito's Contactable
+  # To validate zip codes to German zip code format when country is nil, we return :de format as the default
+  # option when country is nil
+  def zip_country
+    self[:country] || :de
+  end
+
+end
