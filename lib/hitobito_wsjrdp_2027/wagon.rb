e1aaca34f9814cb1122dfde6ec52fb30c37e0383
Felix Maurer <felix@felix-maurer.de>
2025-05-07 01:42:15 +0200

    Signup page without copying the full wizard
    Replace the copied wizard with an included concern that contains just our
    additional logic. Also, make the email address a required field during
    signup as users without email will not be useful to us.

--- lib/hitobito_wsjrdp_2027/wagon.rb
+++ lib/hitobito_wsjrdp_2027/wagon.rb
@@ -20,6 +20,7 @@ module HitobitoWsjrdp2027
       Person.include Wsjrdp2027::Person
       Contactable.prepend Wsjrdp2027::Concerns::Contactable
       Sheet::Person.include Wsjrdp2027::Sheet::Person
+      Wizards::Steps::NewUserForm.include Wsjrdp2027::Wizards::Steps::NewUserForm
     end
 
     initializer 'wsjrdp_2027.add_settings' do |_app|
