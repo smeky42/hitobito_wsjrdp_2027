b65e6898b7e4c2df44a7c732730e97399ca57112
Felix Maurer <felix@felix-maurer.de>
2025-05-29 16:30:15 +0200

    Fix rubocop string quoting complaints

--- hitobito_wsjrdp_2027.gemspec
+++ hitobito_wsjrdp_2027.gemspec
@@ -1,20 +1,20 @@
-$LOAD_PATH.push File.expand_path('../lib', __FILE__)
+$LOAD_PATH.push File.expand_path("../lib", __FILE__)
 
 # Maintain your wagon's version:
-require 'hitobito_wsjrdp_2027/version'
+require "hitobito_wsjrdp_2027/version"
 
 # Describe your gem and declare its dependencies:
 Gem::Specification.new do |s|
   # rubocop:disable SingleSpaceBeforeFirstArg
-  s.name = 'hitobito_wsjrdp_2027'
+  s.name = "hitobito_wsjrdp_2027"
   s.version = HitobitoWsjrdp2027::VERSION
-  s.authors = ['Peter Neubauer']
-  s.email = ['development@smeky.de']
+  s.authors = ["Peter Neubauer"]
+  s.email = ["development@smeky.de"]
   # s.homepage    = 'TODO'
-  s.summary = 'Wsjrdp 2027'
-  s.description = 'Wagon for the German Contingent - World Scout Jamboree 2027'
+  s.summary = "Wsjrdp 2027"
+  s.description = "Wagon for the German Contingent - World Scout Jamboree 2027"
 
-  s.files = Dir['{app,config,db,lib}/**/*'] + ['Rakefile']
-  s.test_files = Dir['test/**/*']
+  s.files = Dir["{app,config,db,lib}/**/*"] + ["Rakefile"]
+  s.test_files = Dir["test/**/*"]
   # rubocop:enable SingleSpaceBeforeFirstArg
 end
--- lib/hitobito_wsjrdp_2027/wagon.rb
+++ lib/hitobito_wsjrdp_2027/wagon.rb
@@ -5,7 +5,7 @@ module HitobitoWsjrdp2027
     include Wagons::Wagon
 
     # Set the required application version.
-    app_requirement '>= 0'
+    app_requirement ">= 0"
 
     # Add a load path for this specific wagon
     config.autoload_paths += %W[
@@ -26,12 +26,12 @@ module HitobitoWsjrdp2027
       PersonSerializer.include Wsjrdp2027::PersonSerializer
     end
 
-    initializer 'wsjrdp_2027.add_settings' do |_app|
-      Settings.add_source!(File.join(paths['config'].existent, 'settings.yml'))
+    initializer "wsjrdp_2027.add_settings" do |_app|
+      Settings.add_source!(File.join(paths["config"].existent, "settings.yml"))
       Settings.reload!
     end
 
-    initializer 'wsjrdp_2027.add_inflections' do |_app|
+    initializer "wsjrdp_2027.add_inflections" do |_app|
       ActiveSupport::Inflector.inflections do |inflect|
         # inflect.irregular 'census', 'censuses'
       end
@@ -40,8 +40,8 @@ module HitobitoWsjrdp2027
     private
 
     def seed_fixtures
-      fixtures = root.join('db', 'seeds')
-      ENV['NO_ENV'] ? [fixtures] : [fixtures, File.join(fixtures, Rails.env)]
+      fixtures = root.join("db", "seeds")
+      ENV["NO_ENV"] ? [fixtures] : [fixtures, File.join(fixtures, Rails.env)]
     end
   end
 end
