50a5d4818e21ddeab4a38943f0aedbe5399e20e5
Felix Maurer <felix@felix-maurer.de>
2025-06-05 01:20:52 +0200

    Remove subscriptions and events from groups
    We don't use the subscriptions and events yet, so the tabs are empty and
    useless at the moment. Hide them for now to avoid confused users. They are
    hidden by setting the abilities so that the events and mailing lists tabs
    on groups are only shown to users if they have a role with permissions on
    the group.

--- lib/hitobito_wsjrdp_2027/wagon.rb
+++ lib/hitobito_wsjrdp_2027/wagon.rb
@@ -24,6 +24,7 @@ module HitobitoWsjrdp2027
       Sheet::Person.include Wsjrdp2027::Sheet::Person
       Wizards::Steps::NewUserForm.include Wsjrdp2027::Wizards::Steps::NewUserForm
       PersonSerializer.include Wsjrdp2027::PersonSerializer
+      GroupAbility.include Wsjrdp2027::GroupAbility
     end
 
     initializer "wsjrdp_2027.add_settings" do |_app|
