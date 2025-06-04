e27b803f0590cb34056af6c4ad1570c04990513d
Jokr97 <36475029+Jokr97@users.noreply.github.com>
2025-06-04 16:35:47 +0200

    add upload tab

---
+++ app/controllers/person/upload_controller.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2025, German Contingent for the Worldscoutjamboree 2027. This file is part of
+#  hitobito_wsjrdp_2027 and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_wsjrdp_2027.
+
+class Person::UploadController < ApplicationController
+  before_action :authorize_action
+  decorates :group, :person
+
+  def index
+    @group ||= Group.find(params[:group_id])
+    @person ||= group.people.find(params[:id])
+    if request.put?
+      flash[:notice] = 'Uploaded: '
+      upload_files
+    end
+  end
+
+  private
+
+  def entry
+    @person ||= Person.find(params[:id])
+  end
+
+  def authorize_action
+    authorize!(:edit, entry)
+  end
+
+  def upload_files
+    unless params[:person].nil?
+      upload_file(params[:person][:upload_contract_pdf], 'upload_contract_pdf')
+      upload_file(params[:person][:upload_data_agreement_pdf], 'upload_data_agreement_pdf')
+      upload_file(params[:person][:upload_passport_pdf], 'upload_passport_pdf')
+      upload_file(params[:person][:upload_good_conduct_pdf], 'upload_good_conduct_pdf')
+    end
+  end
+
+  def upload_file(file_param, file_name)
+    if file_param.nil?
+      return
+    end
+
+    if file_param.content_type == 'application/pdf'
+      file_path = file_path(file_name)
+      FileUtils.mkdir_p(File.dirname(file_path)) unless File.directory?(File.dirname(file_path))
+
+      File.open(file_path, 'wb') do |file|
+        file.write(file_param.read)
+      end
+
+      @person[file_name] = file_path
+      @person.save
+
+    else
+      lash[:alert] = I18n.t('activerecord.alert.only_pdf_allowed')
+    end
+  end
+
+  def file_path(file_name)
+    date = Time.zone.now.strftime('%Y-%m-%d-%H-%M-%S')
+    file_name = file_name.remove('upload_').remove('_pdf')
+    "#{generate_file_path}#{date}--#{@person.id}-#{file_name}.pdf"
+  end
+
+  def generate_file_path
+    "#{Rails.root}/private/uploads/person/pdf/#{@person.id}/"
+  end
+end
---
+++ app/helpers/sheet/person.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2025, German Contingent for the Worldscoutjamboree 2027. This file is part of
+#  hitobito_wsjrdp_2027 and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_wsjrdp_2027.
+
+module Sheet
+  class Person < Base
+    self.parent_sheet = Sheet::Group
+
+    tab "global.tabs.info",
+      :group_person_path,
+      if: :show
+
+    tab "people.tabs.upload",
+      :upload_group_person_path,
+      if: :show
+
+    if Settings.people.abos
+      tab "people.tabs.subscriptions",
+        :group_person_subscriptions_path,
+        if: :show_details
+    end
+
+    tab "people.tabs.invoices",
+      :personal_invoices_group_person_path,
+      if: (lambda do |view, group, person|
+        view.can?(:index_invoices, group) || view.can?(:index_invoices, person)
+      end)
+
+    tab "activerecord.models.message.other",
+      :messages_group_person_path,
+      if: (lambda do |view, _group, person|
+        view.can?(:show_details, person) && (person.roles.any? || person.root?)
+      end)
+
+    tab "people.tabs.history",
+      :history_group_person_path,
+      if: (lambda do |view, _group, person|
+        view.can?(:history, person)
+      end)
+
+    tab "people.tabs.log",
+      :log_group_person_path,
+      if: (lambda do |view, _group, person|
+        view.can?(:log, person)
+      end)
+
+    tab "people.tabs.security_tools",
+      :security_tools_group_person_path,
+      if: (lambda do |view, _group, person|
+        view.can?(:security, person)
+      end)
+
+    tab "people.tabs.colleagues",
+      :colleagues_group_person_path,
+      if: (lambda do |_view, _group, person|
+        person.company_name?
+      end)
+
+    if Settings.assignments&.enabled
+      tab "activerecord.models.assignment.other",
+        :group_person_assignments_path,
+        if: :show_details
+    end
+
+    def link_url
+      view.group_person_path(parent_sheet.entry.id, entry.id)
+    end
+  end
+end
