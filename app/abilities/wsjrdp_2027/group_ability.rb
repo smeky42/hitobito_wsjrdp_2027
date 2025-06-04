66fa4ad072e51a5be760fdc2bfffeb9d51df524a
Felix Maurer <felix@felix-maurer.de>
2025-06-05 01:20:52 +0200

    Remove subscriptions and events from groups
    We don't use the subscriptions and events yet, so the tabs are empty and
    useless at the moment. Hide them for now to avoid confused users. They are
    hidden by setting the abilities so that the events and mailing lists tabs
    on groups are only shown to users if they have a role with permissions on
    the group.

---
+++ app/abilities/wsjrdp_2027/group_ability.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Wsjrdp2027::GroupAbility
+  extend ActiveSupport::Concern
+
+  included do
+    on(Group) do
+      # Show events and mailing lists to users with pemissions to read the group, instead of everyone
+      permission(:any)
+        .may(:index_events, :"index_event/courses", :index_mailing_lists)
+        .nobody
+      permission(:group_read)
+        .may(:index_events, :"index_event/courses", :index_mailing_lists)
+        .in_same_group
+      permission(:group_and_below_read)
+        .may(:index_events, :"index_event/courses", :index_mailing_lists)
+        .in_same_group_or_below
+      permission(:layer_read)
+        .may(:index_events, :"index_event/courses", :index_mailing_lists)
+        .in_same_layer
+      permission(:layer_and_below_read)
+        .may(:index_events, :"index_event/courses", :index_mailing_lists)
+        .in_same_layer_or_below
+    end
+  end
+end
