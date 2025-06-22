82322e6e0152638ec2e6e7b10df6cb604009f343
Peter Neubauer <development@smeky.de>
2025-06-22 16:05:42 +0200

    print contract and payment role

--- app/domain/wsjrdp_2027/export/pdf/registration/contract.rb
+++ app/domain/wsjrdp_2027/export/pdf/registration/contract.rb
@@ -3,34 +3,109 @@
 module Wsjrdp2027
   module Export::Pdf::Registration
     class Contract < Section
-      # include FinanceHelper
+      include ContractHelper
 
       # rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Metrics/CyclomaticComplexity
       def render
         pdf.y = bounds.height - 60
         bounding_box([0, 230.mm], width: bounds.width, height: bounds.height - 200) do
-          font_size(8) do
+          font_size(10) do
             text list, style: :italic, width: bounds.width
           end
         end
       end
 
       def list
-        pdf.move_down 3.mm
+        of_legal_age = @person.years.to_i >= 18
+
         text "Was muss ich mit der Anmeldung machen?", size: 12
         text "Die Anmeldung muss"
         text "1. vollständig unterschrieben werden"
-        text "2. auf anmeldung.worldscoutjamboree.de unter " \
-        + '"Upload>Anmeldung hochladen" hochgeladen werden'
-        text "3. am ersten Treffen der entsprechenden Betreuungsperson im Orginal überreicht werden"
+        text '2. auf anmeldung.worldscoutjamboree.de unter "Upload>Anmeldung hochladen" hochgeladen werden'
+        text "3. am ersten Treffen der entsprechenden Betreuungsperson im Orginal überreicht werden "
         pdf.move_down 3.mm
         pdf.stroke_horizontal_rule
 
         pdf.move_down 3.mm
         text "Anmeldung", size: 12
 
-        text "von " + @person.full_name + ", geboren am " + @person.birthday.strftime("%d.%m.%Y") \
-        + ", wohnhaft in " + @person.address + ", " + @person.zip_code + ", " + @person.town
+        text "von #{@person.full_name} geboren am #{@person.birthday.strftime('%d.%m.%Y')}, wohnhaft in #{@person.address}, #{@person.zip_code}, #{@person.town}"
+        text "beim Ring deutscher Pfadfinder*innenverbände e.V (rdp) Chausseestraße 128/129, 10115 Berlin für das deutsche Kontingent zum 26. World Scout Jamboree 2027 in Polen."
+
+        pdf.move_down 3.mm
+        text "Die Teilnahme, als #{person_payment_role_full_name(@person)}, im deutschen Kontingent kostet #{payment_value(@person)} € und beinhaltet #{role_type(@person).starts_with?("Group::Unit") ? "die Vor- und Nachbereitung in Deutschland, die Reise nach Polen, eine Vor- oder Nachtour " : "die Vor- und Nachbereitung in Deutschland "} und die Teilnahme am 26. World Scout Jamboree in Polen."
+        
+        pdf.move_down 3.mm
+        text "Die Reise ist für den Zeitraum vom 20.07 bis 18.08.2027 geplant. Der Reisezeitraum variiert je nach gewähltem Paket und Lage der Sommerferien. Reisedauer sind #{role_type(@person).starts_with?("Group::Unit") ? "17 bis 21" : "15 bis 17"} Tage."
+
+        pdf.move_down 3.mm
+        text "Hiermit #{@person.years.to_i >= 18 ? "melde ich mich" :  "melden wir unser Kind"}, #{@person.full_name} geboren am #{@person.birthday}, verbindlich mit dem Paket für #{person_payment_role_full_name(@person)} zur Teilnahme im Deutschen Kontingent zum 26. World Scout Jamboree 2027 an. Mit der Anmeldung #{@person.years.to_i >= 18 ? "akzeptiere ich" : "akzeptieren wir"} die Reisebedingungen, die vom rdp als Veranstalter vorgegeben werden."
+        
+        pdf.move_down 3.mm
+        text "Der rdp behält sich das Recht vor, angekündigte Programminhalte durch andere zu ersetzen und notwendige Änderungen des Programms, unter Wahrung des Gesamtcharakters der Veranstaltung vorzunehmen."
+        
+        pdf.move_down 3.mm
+        text "Es besteht die Möglichkeit, dass der Veranstalter des Jamborees in Polen ergänzende Bedingungen für die Teilnahme stellt und/oder weitere Daten abfragt. Der rdp muss diese ergänzenden Bedingungen an alle Teilnehmer*innen weitergeben, obwohl er auf den Inhalt keinen Einfluss hat, weil sonst eine Teilnahme nicht möglich ist. Die Teilnehmer*innen werden über diese Änderungen in Textform unterrichtet. Sollte der Teilnehmer*innen mit diesen ergänzenden Bedingungen nicht einverstanden sein, kann er nach Maßgabe von Punkt 8. der Reisebedingungen zu diesem Vertrag zurücktreten."
+
+        if @person.years.to_i < 18
+          pdf.move_down 3.mm
+          text "Für die Dauer der Reise übertragen wir die Ausübung der Aufsichtspflicht und das Aufenthaltsbestimmungsrecht über unser Kind dem Reiseveranstalter. Wir sind damit einverstanden, dass die Ausübung im erforderlichen Ausmaß auf volljährige Betreuer*innen übertragen wird." 
+        end
+        
+        text "Als Bestandteil dieser Anmeldung haben wir folgende Dokumente in der Anlage zur Kenntnis genommen:"
+        text "- die Teilnahme- und Reisebedingungen des rdp (v1 vom 28.06.2025)"
+        text "- die Datenschutzhinweise (v1 vom 28.06.2025)"
+        text ""
+        text "Die Dokumente stehen auch unter www.worldscoutjamboree.de/downloads zur Verfügung."
+        text "Den Medizinbogen, die Fotohinweise und das SEPA-Mandat im Anhang haben #{@person.years.to_i >= 18 ? "habe ich" : "haben wir"} gesondert unterschrieben."
+        
+        pdf.move_down 3.mm
+        if payment_role(@person).start_with?("EarlyPayer")
+          text "Für die Zahlung des Teilnehmendenbeitrages wünsche ich Einmalzahlung im August 2025."
+        else
+          text "Für die Zahlung des Teilnehmendenbeitrages wünsche Ratenzahlung nach dem in den Reisebedingungen aufgeführten Ratenplan."
+        end 
+
+
+        if @person.years.to_i >= 18
+          signature = pdf.make_table([
+                                       [{ content: @person.town + ' den ' \
+                                        + Time.zone.today.strftime('%d.%m.%Y'), height: 30 }],
+                                       ['______________________________', ''],
+                                       [{ content: @person.full_name, height: 30 }, '']
+                                     ],
+                                     cell_style: { width: 240, padding: 1, border_width: 0,
+                                                   inline_format: true })
+        elsif @person.additional_contact_single
+          signature = pdf.make_table([
+                                       [{ content: @person.town + ' den ' \
+                                         + Time.zone.today.strftime('%d.%m.%Y'), height: 30 }],
+                                       %w(__________________________ __________________________),
+                                       [{ content: @person.additional_contact_name_a, height: 30 },\
+                                        + @person.full_name]
+                                     ],
+                                     cell_style: { width: 240, padding: 1, border_width: 0,
+                                                   inline_format: true })
+        else
+          signature = pdf.make_table([
+                                       [{ content: @person.town + ' den ' \
+                                         + Time.zone.today.strftime('%d.%m.%Y'), height: 30 }],
+                                       %w(__________________________ __________________________),
+                                       [{ content: @person.additional_contact_name_a, height: 30 },\
+                                        + @person.additional_contact_name_b],
+                                       ['______________________________', ''],
+                                       [{ content: @person.full_name, height: 30 }, '']
+                                     ],
+                                     cell_style: { width: 240, padding: 1, border_width: 0,
+                                                   inline_format: true })
+        end
+
+        pdf.move_down 3.mm
+
+        pdf.move_down 3.mm
+
+        signature.draw
+
 
         pdf.start_new_page
         pdf.move_down 3.mm
---
+++ app/helpers/contract_helper.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+module ContractHelper
+  extend ActiveSupport::Concern
+
+  included do
+    # rubocop:disable Metrics/MethodLength
+    def payment_array
+      [
+        ["Rolle","Gesamtbeitrag","Dez 2025 (Anzahlung)","Jan 2026","Feb 2026","Mär 2026","Aug 2026","Nov 2026","Feb 2027","Mai 2027"],
+        ["RegularPayer::Group::Unit::Member","3400","300","500","500","500","400","400","400","400"],
+        ["RegularPayer::Group::Unit::Leader","2400","150","350","350","350","300","300","300","300"],
+        ["RegularPayer::Group::Ist::Member","2600","200","400","400","400","300","300","300","300"],
+        ["RegularPayer::Group::Root::Member","1600","50","250","250","250","200","200","200","200"],
+        ["EarlyPayer::Group::Unit::Member","3400","","","","","","","",""],
+        ["EarlyPayer::Group::Unit::Leader","2400","","","","","","","",""],
+        ["EarlyPayer::Group::Ist::Member","2600","","","","","","","",""],
+        ["EarlyPayer::Group::Root::Member","1600","","","","","","","",""]
+      ]
+    end
+    # rubocop:enable Metrics/MethodLength
+
+    # each person has a primary group which defines the price and role type
+    def role_type(person) 
+      roles = person.current_roles_grouped
+      # If no role could be detected, fallback should be Youth Participant
+      role = "Group::Unit::Member"
+
+      # Last assigned role_type in primary group 
+      roles.each do |key, value|
+        value.each do |item|
+          if item.group_id == person.primary_group_id
+            role = item.type
+          end
+        end
+      end
+      role 
+    end
+
+    def role_full_name(role)
+      I18n.t("people.print.contract_roles.#{role.gsub("::", ".")}")
+    end
+
+    def payment_role_full_name(role)
+      role_full_name(role.split("::", 2)[1])
+    end
+
+    def person_payment_role_full_name(person)
+      role = payment_role(person)
+      role_full_name(role.split("::", 2)[1])
+    end
+
+    def payment_role(person)
+      role = role_type(person)
+      payment_role_name = "RegularPayer"
+
+      if(person.early_payer)
+        payment_role_name = "EarlyPayer"
+      end
+
+      if((role == "Group::Unit::UnapprovedLeader") or (role == "Group::Unit::Leader"))
+        payment_role_name += "::Group::Unit::Leader"
+      elsif(role == "Group::Unit::Member")
+        payment_role_name += "::Group::Unit::Member"
+      elsif(role == "Group::Ist::Member") 
+        payment_role_name += "Group::Ist::Member"
+      else
+        payment_role_name += "Group::Root::Member"
+      end
+      payment_role_name      
+    end
+    
+    def payment_array_by(role)
+      payment_array.find { |row| row[0] == role }
+    end
+
+    def payment_value(person)
+      payment_array_by(person.payment_role)[1]
+    end
+ 
+  end
+end
