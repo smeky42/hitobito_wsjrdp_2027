9aacf6f993c9497d21e1c6c08ab15ade0dccc644
Peter Neubauer <development@smeky.de>
2025-06-22 21:45:32 +0200

    rubocop

--- app/domain/wsjrdp_2027/export/pdf/registration/contract.rb
+++ app/domain/wsjrdp_2027/export/pdf/registration/contract.rb
@@ -29,75 +29,73 @@ module Wsjrdp2027
         pdf.move_down 3.mm
         text "Anmeldung", size: 12
 
-        text "von #{@person.full_name} geboren am #{@person.birthday.strftime('%d.%m.%Y')}, wohnhaft in #{@person.address}, #{@person.zip_code}, #{@person.town}"
+        text "von #{@person.full_name} geboren am #{@person.birthday.strftime("%d.%m.%Y")}, wohnhaft in #{@person.address}, #{@person.zip_code}, #{@person.town}"
         text "beim Ring deutscher Pfadfinder*innenverbände e.V (rdp) Chausseestraße 128/129, 10115 Berlin für das deutsche Kontingent zum 26. World Scout Jamboree 2027 in Polen."
 
         pdf.move_down 3.mm
         text "Die Teilnahme, als #{person_payment_role_full_name(@person)}, im deutschen Kontingent kostet #{payment_value(@person)} € und beinhaltet #{role_type(@person).starts_with?("Group::Unit") ? "die Vor- und Nachbereitung in Deutschland, die Reise nach Polen, eine Vor- oder Nachtour " : "die Vor- und Nachbereitung in Deutschland "} und die Teilnahme am 26. World Scout Jamboree in Polen."
-        
+
         pdf.move_down 3.mm
         text "Die Reise ist für den Zeitraum vom 20.07 bis 18.08.2027 geplant. Der Reisezeitraum variiert je nach gewähltem Paket und Lage der Sommerferien. Reisedauer sind #{role_type(@person).starts_with?("Group::Unit") ? "17 bis 21" : "15 bis 17"} Tage."
 
         pdf.move_down 3.mm
-        text "Hiermit #{@person.years.to_i >= 18 ? "melde ich mich" :  "melden wir unser Kind"}, #{@person.full_name} geboren am #{@person.birthday}, verbindlich mit dem Paket für #{person_payment_role_full_name(@person)} zur Teilnahme im Deutschen Kontingent zum 26. World Scout Jamboree 2027 an. Mit der Anmeldung #{@person.years.to_i >= 18 ? "akzeptiere ich" : "akzeptieren wir"} die Reisebedingungen, die vom rdp als Veranstalter vorgegeben werden."
-        
+        text "Hiermit #{of_legal_age ? "melde ich mich" : "melden wir unser Kind"}, #{@person.full_name} geboren am #{@person.birthday}, verbindlich mit dem Paket für #{person_payment_role_full_name(@person)} zur Teilnahme im Deutschen Kontingent zum 26. World Scout Jamboree 2027 an. Mit der Anmeldung #{of_legal_age ? "akzeptiere ich" : "akzeptieren wir"} die Reisebedingungen, die vom rdp als Veranstalter vorgegeben werden."
+
         pdf.move_down 3.mm
         text "Der rdp behält sich das Recht vor, angekündigte Programminhalte durch andere zu ersetzen und notwendige Änderungen des Programms, unter Wahrung des Gesamtcharakters der Veranstaltung vorzunehmen."
-        
+
         pdf.move_down 3.mm
         text "Es besteht die Möglichkeit, dass der Veranstalter des Jamborees in Polen ergänzende Bedingungen für die Teilnahme stellt und/oder weitere Daten abfragt. Der rdp muss diese ergänzenden Bedingungen an alle Teilnehmer*innen weitergeben, obwohl er auf den Inhalt keinen Einfluss hat, weil sonst eine Teilnahme nicht möglich ist. Die Teilnehmer*innen werden über diese Änderungen in Textform unterrichtet. Sollte der Teilnehmer*innen mit diesen ergänzenden Bedingungen nicht einverstanden sein, kann er nach Maßgabe von Punkt 8. der Reisebedingungen zu diesem Vertrag zurücktreten."
 
-        if @person.years.to_i < 18
+        if !of_legal_age
           pdf.move_down 3.mm
-          text "Für die Dauer der Reise übertragen wir die Ausübung der Aufsichtspflicht und das Aufenthaltsbestimmungsrecht über unser Kind dem Reiseveranstalter. Wir sind damit einverstanden, dass die Ausübung im erforderlichen Ausmaß auf volljährige Betreuer*innen übertragen wird." 
+          text "Für die Dauer der Reise übertragen wir die Ausübung der Aufsichtspflicht und das Aufenthaltsbestimmungsrecht über unser Kind dem Reiseveranstalter. Wir sind damit einverstanden, dass die Ausübung im erforderlichen Ausmaß auf volljährige Betreuer*innen übertragen wird."
         end
-        
+
         text "Als Bestandteil dieser Anmeldung haben wir folgende Dokumente in der Anlage zur Kenntnis genommen:"
         text "- die Teilnahme- und Reisebedingungen des rdp (v1 vom 28.06.2025)"
         text "- die Datenschutzhinweise (v1 vom 28.06.2025)"
         text ""
         text "Die Dokumente stehen auch unter www.worldscoutjamboree.de/downloads zur Verfügung."
-        text "Den Medizinbogen, die Fotohinweise und das SEPA-Mandat im Anhang haben #{@person.years.to_i >= 18 ? "habe ich" : "haben wir"} gesondert unterschrieben."
-        
+        text "Den Medizinbogen, die Fotohinweise und das SEPA-Mandat im Anhang haben #{of_legal_age ? "habe ich" : "haben wir"} gesondert unterschrieben."
+
         pdf.move_down 3.mm
         if payment_role(@person).start_with?("EarlyPayer")
           text "Für die Zahlung des Teilnehmendenbeitrages wünsche ich Einmalzahlung im August 2025."
         else
           text "Für die Zahlung des Teilnehmendenbeitrages wünsche Ratenzahlung nach dem in den Reisebedingungen aufgeführten Ratenplan."
-        end 
+        end
 
-
-        if @person.years.to_i >= 18
-          signature = pdf.make_table([
-                                       [{ content: @person.town + ' den ' \
-                                        + Time.zone.today.strftime('%d.%m.%Y'), height: 30 }],
-                                       ['______________________________', ''],
-                                       [{ content: @person.full_name, height: 30 }, '']
-                                     ],
-                                     cell_style: { width: 240, padding: 1, border_width: 0,
-                                                   inline_format: true })
+        signature = if of_legal_age
+          pdf.make_table([
+            [{content: @person.town + " den #{Time.zone.today.strftime("%d.%m.%Y")}", height: 30}],
+            ["______________________________", ""],
+            [{content: @person.full_name, height: 30}, ""]
+          ],
+            cell_style: {width: 240, padding: 1, border_width: 0,
+                         inline_format: true})
         elsif @person.additional_contact_single
-          signature = pdf.make_table([
-                                       [{ content: @person.town + ' den ' \
-                                         + Time.zone.today.strftime('%d.%m.%Y'), height: 30 }],
-                                       %w(__________________________ __________________________),
-                                       [{ content: @person.additional_contact_name_a, height: 30 },\
-                                        + @person.full_name]
-                                     ],
-                                     cell_style: { width: 240, padding: 1, border_width: 0,
-                                                   inline_format: true })
+          pdf.make_table([
+            [{content: @person.town + " den " \
+              + Time.zone.today.strftime("%d.%m.%Y"), height: 30}],
+            %w[__________________________ __________________________],
+            [{content: @person.additional_contact_name_a, height: 30}, \
+              + @person.full_name]
+          ],
+            cell_style: {width: 240, padding: 1, border_width: 0,
+                         inline_format: true})
         else
-          signature = pdf.make_table([
-                                       [{ content: @person.town + ' den ' \
-                                         + Time.zone.today.strftime('%d.%m.%Y'), height: 30 }],
-                                       %w(__________________________ __________________________),
-                                       [{ content: @person.additional_contact_name_a, height: 30 },\
-                                        + @person.additional_contact_name_b],
-                                       ['______________________________', ''],
-                                       [{ content: @person.full_name, height: 30 }, '']
-                                     ],
-                                     cell_style: { width: 240, padding: 1, border_width: 0,
-                                                   inline_format: true })
+          pdf.make_table([
+            [{content: @person.town + " den " \
+              + Time.zone.today.strftime("%d.%m.%Y"), height: 30}],
+            %w[__________________________ __________________________],
+            [{content: @person.additional_contact_name_a, height: 30}, \
+              + @person.additional_contact_name_b],
+            ["______________________________", ""],
+            [{content: @person.full_name, height: 30}, ""]
+          ],
+            cell_style: {width: 240, padding: 1, border_width: 0,
+                         inline_format: true})
         end
 
         pdf.move_down 3.mm
@@ -105,7 +103,6 @@ module Wsjrdp2027
         pdf.move_down 3.mm
 
         signature.draw
-
 
         pdf.start_new_page
         pdf.move_down 3.mm
@@ -120,55 +117,54 @@ module Wsjrdp2027
         pdf.move_down 3.mm
 
         text "SEPA-Mandat", size: 12
-      
+
         text "Die Teilnahmebeitrag zum Jamboree werden mittels SEPA-Basislastschrift eingezogen:"
-        
+
         pdf.move_down 3.mm
         text "Ich ermächtige den Ring deutscher Pfadfinder*innenverbände e.V., die Zahlungen gemäß Zahlungsplan von meinem Konto mittels Lastschrift einzuziehen. Maßgeblich ist das von mir gewählte Zahlungsmodell (#{payment_role(@person).start_with?("EarlyPayer") ? "Einmalzahlung" : "Ratenzahlungen"}). Zugleich weise ich mein Kreditinstitut an, die vom Ring deutscher Pfadfinder*innenverbände e.V. auf mein Konto gezogenen Lastschriften einzulösen."
-        
+
         pdf.move_down 3.mm
         text "Hinweis: Ich kann innerhalb von acht Wochen, beginnend mit dem Belastungsdatum, die Erstattung des belasteten Betrages verlangen."
         text "Es gelten dabei die mit meinem Kreditinstitut vereinbarten Bedingungen."
-        
+
         pdf.move_down 3.mm
         attendee_data = pdf.make_table([
-                                         [{ content: 'IBAN:', width: 150 }, @person.sepa_iban],
-                                         ['Mandatsreferenz:', 'wsjrdp2027' + @person.id.to_s],
-                                         ['Gläubiger*innen-Identifikationsnummer:',
-                                          'DE81 WSJ 0000 2017 275'], # Todo
-                                         ['Kontoinhaber*in:', @person.sepa_name],
-                                         ['Adresse:', @person.sepa_address]
-                                       ],
-                                       cell_style: { padding: 1, border_width: 0,
-                                                     inline_format: true })
+          [{content: "IBAN:", width: 150}, @person.sepa_iban],
+          ["Mandatsreferenz:", "wsjrdp2027" + @person.id.to_s],
+          ["Gläubiger*innen-Identifikationsnummer:",
+            "DE81 WSJ 0000 2017 275"], # Todo
+          ["Kontoinhaber*in:", @person.sepa_name],
+          ["Adresse:", @person.sepa_address]
+        ],
+          cell_style: {padding: 1, border_width: 0,
+                       inline_format: true})
         attendee_data.draw
 
         pdf.move_down 3.mm
 
         if payment_role(@person).start_with?("EarlyPayer")
           text "Der Einzug des Gesamtbetrages von #{payment_value(@person)} € erfolgt am 5. August 2025."
-        else 
+        else
           text "Der Einzug erfolgt am 5. des jeweiligen Monats bzw. am darauffolgenden Werktag nach folgendem Ratenplan:"
-          pdf.make_table(payment_array_table(@person), cell_style: { padding: 1,
-                                                                            border_width: 0,
-                                                                            inline_format: true,
-                                                                            size: 8 }).draw
+          pdf.make_table(payment_array_table(@person), cell_style: {padding: 1,
+                                                                    border_width: 0,
+                                                                    inline_format: true,
+                                                                    size: 8}).draw
         end
-        
+
         pdf.move_down 3.mm
         text "Die Anzahlung wird frühestens nach dem Hochladen aller Anmeldeunterlagen eingezogen. Wir werden mindestens 7 Tage vor dem Einzug der Anzahlung über diesen informieren"
         text "Im Falle einer Rücklastschrift behalten wir uns vor, die dadurch entstehenden Kosten an die*den Teilnehmer*in weiterzugeben."
 
-       
         pdf.move_down 3.mm
         pdf.make_table([
-                         [{ content: @person.town + ' den ' + Time.zone.today.strftime('%d.%m.%Y'),
-                            height: 30 }],
-                         ['______________________________', ''],
-                         [{ content: @person.sepa_name, height: 30 }, '']
-                       ],
-                       cell_style: { width: 240, padding: 1, border_width: 0,
-                                     inline_format: true }).draw
+          [{content: @person.town + " den " + Time.zone.today.strftime("%d.%m.%Y"),
+            height: 30}],
+          ["______________________________", ""],
+          [{content: @person.sepa_name, height: 30}, ""]
+        ],
+          cell_style: {width: 240, padding: 1, border_width: 0,
+                       inline_format: true}).draw
 
         text ""
       end
