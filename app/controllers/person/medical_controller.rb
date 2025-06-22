f542a68b1eae3592403a858a55b9124f0ef59d09
Felix Maurer <felix@felix-maurer.de>
2025-06-22 18:31:11 +0200

    Add edit and update functionality for medical information

--- app/controllers/person/medical_controller.rb
+++ app/controllers/person/medical_controller.rb
@@ -1,11 +1,44 @@
 # frozen_string_literal: true
 
 class Person::MedicalController < ApplicationController
+  class_attribute :permitted_attrs
+
   respond_to :html
+
+  self.permitted_attrs = [
+    :medical_stiko_vaccinations,
+    :medical_additional_vaccinations,
+    :medical_preexisting_conditions,
+    :medical_abnormalities,
+    :medical_allergies,
+    :medical_eating_disorders,
+    :medical_mobility_needs,
+    :medical_infectious_diseases,
+    :medical_medical_treatment_contact,
+    :medical_continuous_medication,
+    :medical_needs_medication,
+    :medical_self_treatment_medication,
+    :medical_mental_health,
+    :medical_situational_support,
+    :medical_person_of_trust,
+    :medical_other
+  ]
 
   def show
     authorize!(:show_full, person)
     render "show"
+  end
+
+  def edit
+    authorize!(:edit, person)
+    render "edit"
+  end
+
+  def update
+    authorize!(:edit, person)
+    person.attributes = params.require(:person).permit(permitted_attrs)
+    person.save
+    respond_with person, location: medical_group_person_path
   end
 
   private
