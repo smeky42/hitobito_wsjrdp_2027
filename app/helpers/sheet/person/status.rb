681c9c9b998794cb336d0c4c85d11869dcd1e1b1
Peter Neubauer <development@smeky.de>
2025-06-23 17:56:23 +0200

    added tab to check status and documents

---
+++ app/helpers/sheet/person/status.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Sheet
+  class Person < Base
+    class Status < Base
+      class_attribute :always_render_parent
+
+      self.parent_sheet = Sheet::Person
+      self.always_render_parent = true
+    end
+  end
+end
