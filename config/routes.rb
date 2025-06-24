ce4b6e71dac0c82c7452316abe5568888a6787a5
Peter Neubauer <development@smeky.de>
2025-06-24 18:19:03 +0200

    extra upload photo and good conduct updated rules

--- config/routes.rb
+++ config/routes.rb
@@ -18,11 +18,13 @@ Rails.application.routes.draw do
           get "upload" => "person/upload#index"
           put "upload" => "person/upload#index"
           get "upload/show_contract" => "person/upload#show_contract"
-          get "upload/show_contract" => "person/upload#show_contract"
           get "upload/show_medical" => "person/upload#show_medical"
           get "upload/show_data_agreement" => "person/upload#show_data_agreement"
           get "upload/show_passport" => "person/upload#show_passport"
           get "upload/show_recommendation" => "person/upload#show_recommendation"
+          get "upload/show_data_agreement" => "person/upload#show_data_agreement"
+          get "upload/show_photo_permission" => "person/upload#show_photo_permission"
+
 
           get "medical" => "person/medical#show"
           get "medical/edit" => "person/medical#edit"
