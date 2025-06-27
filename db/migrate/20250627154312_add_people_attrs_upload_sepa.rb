a8ce6f165e4d0f3fea5ff8114539e24a35a580d3
Peter Neubauer <development@smeky.de>
2025-06-27 15:49:50 +0200

    sepa upload and fix show photo

---
+++ db/migrate/20250627154312_add_people_attrs_upload_sepa.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2025, German Contingent for the Worldscoutjamboree 2027. This file is part of
+#  hitobito_wsjrdp_2027 and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_wsjrdp_2027.
+
+class AddPeopleAttrsUploadSepa < ActiveRecord::Migration[4.2]
+  def change
+    add_column :people, :upload_sepa_pdf, :string
+  end
+end
