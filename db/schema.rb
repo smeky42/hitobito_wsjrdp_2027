acdeb0d98b4538134561ea7dc4641ff8bca30b31
David Fritzsche <dfritzsche@daffi.net>
2025-10-17 09:23:16 +0200

    Small fixes
    * Extend .gitigore to not ignore the .github directory (useful for
      search party tools)
    * Update db/schema.rb and db/schema.rb.diff for previous migration
    * Update name of BdP: Bayern: Keine Bezirksebene: Steinadler
      to BdP: Bayern: Keine Bezirksebene: Steinadler München

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_26_141500) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_04_103100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -1050,6 +1050,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_26_141500) do
     t.string "diet", default: "omnivorous"
     t.string "upload_sepa_pdf"
     t.date "print_at"
+    t.string "longitude"
+    t.string "latitude"
+    t.string "unit_code"
     t.virtual "search_column", type: :tsvector, as: "to_tsvector('simple'::regconfig, ((((((((((((((((((((((COALESCE((first_name)::text, ''::text) || ' '::text) || COALESCE((last_name)::text, ''::text)) || ' '::text) || COALESCE((company_name)::text, ''::text)) || ' '::text) || COALESCE((nickname)::text, ''::text)) || ' '::text) || COALESCE((email)::text, ''::text)) || ' '::text) || COALESCE((street)::text, ''::text)) || ' '::text) || COALESCE((housenumber)::text, ''::text)) || ' '::text) || COALESCE((zip_code)::text, ''::text)) || ' '::text) || COALESCE((town)::text, ''::text)) || ' '::text) || COALESCE((country)::text, ''::text)) || ' '::text) ||\nCASE\n    WHEN (birthday IS NOT NULL) THEN (((((EXTRACT(year FROM birthday))::text || '-'::text) || lpad((EXTRACT(month FROM birthday))::text, 2, '0'::text)) || '-'::text) || lpad((EXTRACT(day FROM birthday))::text, 2, '0'::text))\n    ELSE ''::text\nEND) || ' '::text) || COALESCE(additional_information, ''::text)))", stored: true
     t.index ["authentication_token"], name: "index_people_on_authentication_token"
     t.index ["confirmation_token"], name: "index_people_on_confirmation_token", unique: true
