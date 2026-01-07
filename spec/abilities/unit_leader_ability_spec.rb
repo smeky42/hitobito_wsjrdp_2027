6f1b48c298ade8a1f21e201593660454cffdee84
Felix Maurer <felix@felix-maurer.de>
2026-01-07 01:07:18 +0100

    tests: add more permission tests for ULs

--- spec/abilities/unit_leader_ability_spec.rb
+++ spec/abilities/unit_leader_ability_spec.rb
@@ -11,14 +11,51 @@ describe PersonAbility do
       let(:other_ul) { people(:ul_a_2) }
 
       context "on youth participant" do
-        it_behaves_like "only allow actions", {allowed: [:show]} do
+        it_behaves_like "only allow actions", {allowed: [:show, :show_details]} do
           let(:other) { other_yp }
         end
       end
 
       context "on other unit leader" do
-        it_behaves_like "only allow actions", {allowed: [:show]} do
+        it_behaves_like "only allow actions", {allowed: [:show, :show_details]} do
           let(:other) { other_ul }
+        end
+      end
+    end
+
+    context "in a different unit" do
+      let(:other_yp) { people(:yp_b_1) }
+      let(:other_ul) { people(:ul_b_1) }
+
+      context "on youth participant" do
+        it_behaves_like "only allow actions", {allowed: []} do
+          let(:other) { other_yp }
+        end
+      end
+
+      context "on unit leader" do
+        it_behaves_like "only allow actions", {allowed: []} do
+          let(:other) { other_ul }
+        end
+      end
+    end
+
+    context "CMT" do
+      let(:cmt) { people(:cmt_member1) }
+
+      context "on CMT member" do
+        it_behaves_like "only allow actions", {allowed: []} do
+          let(:other) { cmt }
+        end
+      end
+    end
+
+    context "IST" do
+      let(:ist) { people(:ist_a_1) }
+
+      context "on IST member" do
+        it_behaves_like "only allow actions", {allowed: []} do
+          let(:other) { ist }
         end
       end
     end
