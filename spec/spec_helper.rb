b06792c6769a718af6687fcd62e59979fc3d2fbb
Felix Maurer <felix@felix-maurer.de>
2026-01-11 15:29:33 +0100

    tests: Fix fixture loading
    We need to fully configure RSpec (including the fixtures) before loading
    the support files, because our support files with the shared examples also
    include the example/test to test that we cover all actions. Previously,
    when one of these tests did run before any of the actual ability tests, the
    fixtures from Hitobito core were loaded and cached, because the
    configuration for the wagon did only happen afterwards.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,10 +5,10 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../Gemfile", __FILE__)
 
 require File.join(ENV["APP_ROOT"], "spec", "spec_helper.rb")
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[HitobitoWsjrdp2027::Wagon.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
-
 RSpec.configure do |config|
   config.fixture_paths = [File.expand_path("../fixtures", __FILE__)]
 end
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[HitobitoWsjrdp2027::Wagon.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
