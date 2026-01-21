aa97c543bc5fa394d3c85fe5b6a05e8476bfe162
David Fritzsche <dfritzsche@daffi.net>
2026-01-21 02:58:15 +0100

    Fix group editing

--- app/models/wsjrdp_2027/group.rb
+++ app/models/wsjrdp_2027/group.rb
@@ -3,6 +3,8 @@
 module Wsjrdp2027::Group
   extend ActiveSupport::Concern
 
+  DELETE_IF_BLANK_ATTRS = [:support_cmt_mail_addresses, :unit_code].freeze
+
   included do
     # Define additional used attributes
     # self.used_attributes += [:website, :bank_account, :description]
@@ -13,12 +15,31 @@ module Wsjrdp2027::Group
     store_accessor :additional_info, :unit_code
     store_accessor :additional_info, :support_cmt_mail_addresses
 
+    before_save :normalize_additional_info_attrs
+
     def support_cmt_mail_addresses_string
-      support_cmt_mail_addresses.join("\n")
+      support_cmt_mail_addresses&.join("\n")
     end
 
     def support_cmt_mail_addresses_string=(value)
-      self.support_cmt_mail_addresses = value.tr("\n", ",").split(",").map { |s| s.strip }.compact
+      addresses = (value || "").tr("\n", ",").split(",").map { |s| s.strip.presence }.compact
+      if addresses.blank?
+        additional_info.delete("support_cmt_mail_addresses")
+      else
+        self.support_cmt_mail_addresses = addresses
+      end
+    end
+
+    private
+
+    def normalize_additional_info_attrs
+      [:unit_code].each do |attr|
+        val = send(attr)
+        send(:"#{attr}=", val.strip) if val.respond_to?(:strip)
+      end
+      DELETE_IF_BLANK_ATTRS.each do |attr|
+        additional_info.delete(attr.to_s) if send(attr).blank?
+      end
     end
   end
 end
