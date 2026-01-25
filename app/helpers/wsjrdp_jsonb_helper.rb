d716d8e4e09b553d4fd5be8d19b4c3558ca2d9d8
David Fritzsche <dfritzsche@daffi.net>
2026-01-25 19:10:13 +0100

    Add jsonb_accessor helper method

---
+++ app/helpers/wsjrdp_jsonb_helper.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2026 German Contingent for the World Scout Jamboree 2027.
+#
+#  This file is part of hitobito_wsjrdp_2027 and licensed under the
+#  Affero General Public License version 3 or later. See the COPYING
+#  file at the top-level directory or at
+#  https://github.com/smeky42/hitobito_wsjrdp_2027
+
+module WsjrdpJsonbHelper
+  extend ActiveSupport::Concern
+  include ActiveRecord::Store
+
+  module ClassMethods
+    def jsonb_accessor(store_attribute, key, prefix: nil, suffix: nil, strip: false, delete_on_blank: true)
+      accessor_prefix =
+        case prefix
+        when String, Symbol
+          "#{prefix}_"
+        when TrueClass
+          "#{store_attribute}_"
+        else
+          ""
+        end
+      accessor_suffix =
+        case suffix
+        when String, Symbol
+          "_#{suffix}"
+        when TrueClass
+          "_#{store_attribute}"
+        else
+          ""
+        end
+      accessor_key = "#{accessor_prefix}#{key}#{accessor_suffix}"
+      store_accessor(store_attribute, key, prefix: prefix, suffix: suffix)
+      store_accessor_module = instance_method(:"#{accessor_key}=").owner
+      store_accessor_module.module_eval do
+        define_method(:"#{accessor_key}=") do |value|
+          value = value&.strip if strip
+          if delete_on_blank && value.blank?
+            send(store_attribute).delete(accessor_key)
+          else
+            write_store_attribute(store_attribute, key, value)
+          end
+        end
+      end
+    end
+  end
+end
